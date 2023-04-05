48add352b312423f6289cb60ea5e0eec372d83c8
wellen <wellenhan45@gmail.com>
2023-04-05 23:07:37 +0800

    update

---
+++ app/src/main/java/com/example/myplanet/DataAstronaut.kt
@@ -0,0 +1,11 @@
+package com.example.myplanet
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class DataAstronaut (
+    var PhotoAstronaut : Int,
+    var nameAstronaut : String,
+    var descAstronaut : String
+): Parcelable
